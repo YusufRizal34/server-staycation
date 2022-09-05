5a9433d1c48e42296d350cd04b953adf9947987c
Muhammad Yusuf Rizal <myusufrizal34@gmail.com>
2022-09-05 10:18:08 +0700

    delete cors config

--- app.js
+++ app.js
@@ -25,19 +25,6 @@ const apiRouter = require("./routes/api");
 
 var app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
