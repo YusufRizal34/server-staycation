f6142fc6293612f6d4f1ecc6ac9ad8037e92a379
Muhammad Yusuf Rizal <myusufrizal34@gmail.com>
2022-09-05 08:52:03 +0700

    change testimonial image

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -31,7 +31,7 @@ module.exports = {
 
       const testimonial = {
         _id: "asd1293uasdads1",
-        imageUrl: "/images/testimonial-landingpages.jpg",
+        imageUrl: "/images/testimonial2.jpg",
         name: "Happy Family",
         rate: 4.55,
         content:
